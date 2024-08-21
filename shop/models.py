a5ecb7fd656293a9bcc8e8986430622f15b9b9be
meetvivek <kumarvivek.july28@gmail.com>
2024-08-21 07:53:37 +0530

    Added view and URL for product listing

--- shop/models.py
+++ shop/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 
 # Create your models here.
-class Products(models.Model):
+class Product(models.Model):
+    def __str__(self):
+        return self.title 
+
     title = models.CharField(max_length=200)
     price = models.FloatField()
     discount_price = models.FloatField()
