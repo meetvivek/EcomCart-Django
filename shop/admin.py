3717591ff84c5fda3bbb7f1450786f2b6cc37d04
meetvivek <kumarvivek.july28@gmail.com>
2024-09-17 20:31:06 +0530

    Customizing the Djnago Admin Panel

--- shop/admin.py
+++ shop/admin.py
@@ -2,5 +2,21 @@ from django.contrib import admin
 from .models import Product, Order
 # Register your models here.
 
-admin.site.register(Product)
+admin.site.site_header = "EcomCart"
+admin.site.site_title = "EcomCart"
+admin.site.index_title = "Manage EcomCart"
+
+class ProductAdmin (admin.ModelAdmin):
+    def change_category_to_default(self, request, queryset):
+        queryset.update(category = "default")
+
+    change_category_to_default.short_description = "Default Category"
+
+    list_display = ('title', 'price', 'discount_price', 'category', 'description')
+    search_fields = ('title', 'category' )
+    actions = (change_category_to_default, )
+    list_editable = ('price', 'discount_price')
+    # fields = ('title', 'price',)
+
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Order)
